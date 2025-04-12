875b7511cd3fa312030d5151fd230586196fc9a0
alexphanna <ahanna0608@gmail.com>
2025-04-12 15:58:45 -0400

    Add project titles

---
+++ main.ts
@@ -0,0 +1,65 @@
+const projects = 5;
+const projectTitles = [
+  "RU Eating",
+  "Frontier",
+  "Blackjack",
+  "Workouts",
+  "Chess"
+]
+const projectSubtitles = [
+  "An unofficial Rutgers University dining hall menu app for iOS written in Swift using SwiftUI, SwiftData and SwiftSoup.",
+  "A web-based multiplayer game inspired by the game mechanics of Catan.",
+  "Blackjack in JavaScript. Made for AP Computer Science Principles in senior year of high school.",
+  "Workout planning app for iOS in Swift using SwiftUI and CoreData.",
+  "Console App Chess in C#. Made for fun in junior year of high school."
+]
+const projectLinks = [
+  "https://github.com/alexphanna/ru-eating",
+  "https://github.com/alexphanna/frontier",
+  "https://github.com/alexphanna/blackjack",
+  "https://github.com/alexphanna/workouts",
+  "https://github.com/alexphanna/chess"
+]
+
+const discs: HTMLElement = document.getElementById("disc-container")!;
+
+for (let i = 0; i < projects; i++) {
+  let disc = document.createElement("div");
+  disc.className = "disc";
+  disc.id = i + "";
+  if (i != 0) {
+    disc.style.marginTop = "-30vh";
+  }
+
+  const images = ["cd"];
+  disc.style.backgroundImage =
+    "url(images/" + images[Math.floor(Math.random() * images.length)] + ".png)";
+
+  const angle = Math.random() * 90 - 45;
+  disc.style.transform = "Rotate(" + angle + "deg)";
+
+  const discolor = Math.floor(Math.random() * 360);
+  disc.style.filter = "sepia(10%) hue-rotate(" + discolor + "deg)"
+
+  disc.addEventListener("mouseenter", function () {
+    disc.style.marginLeft = "-10vh";
+    updateProject(i);
+  });
+  disc.addEventListener("mouseleave", function () {
+    disc.style.marginLeft = "-20vh";
+  });
+  disc.addEventListener("mousedown", function () {
+    window.location.href = projectLinks[i];
+  });
+
+  discs.appendChild(disc);
+}
+
+updateProject(0);
+
+function updateProject(i: number) {
+  const projectTitle = document.getElementById("project-title")!;
+  projectTitle.innerHTML = "<a href=" + projectLinks[i] + ">" + projectTitles[i].toLowerCase() + "</a>";
+  const projectSubtitle = document.getElementById("project-subtitle")!;
+  projectSubtitle.innerHTML = projectSubtitles[i];
+}
--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 "use strict";
-const discs = document.getElementById("disc-container");
-let amount = 5;
-for (let i = 0; i < amount; i++) {
-    let disc = document.createElement("div");
-    disc.className = "disc";
-    disc.style.zIndex = "" + i;
-    disc.style.marginTop = i * 10 + "vh";
-    const images = ["cd"];
-    disc.style.backgroundImage = "url(images/" + images[Math.floor(Math.random() * images.length)] + ".png)";
-    const angle = Math.random() * 90 - 45;
-    disc.style.transform = "Rotate(" + angle + "deg)";
-    disc.addEventListener("mouseenter", function () {
-        disc.style.marginLeft = "-10vw";
-    });
-    disc.addEventListener("mouseleave", function () {
-        disc.style.marginLeft = "-20vw";
-    });
-    discs.appendChild(disc);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.main = main;
+const projects_json_1 = __importDefault(require("./projects.json"));
+function main() {
+    const discs = document.getElementById("disc-container");
+    let amount = 5;
+    for (let i = 0; i < amount; i++) {
+        let disc = document.createElement("div");
+        disc.className = "disc";
+        disc.style.zIndex = "" + i;
+        if (i != 0) {
+            disc.style.marginTop = "-30vh";
+        }
+        const images = ["cd"];
+        disc.style.backgroundImage =
+            "url(images/" + images[Math.floor(Math.random() * images.length)] + ".png)";
+        const angle = Math.random() * 90 - 45;
+        disc.style.transform = "Rotate(" + angle + "deg)";
+        disc.addEventListener("mouseenter", function () {
+            disc.style.marginLeft = "-10vh";
+        });
+        disc.addEventListener("mouseleave", function () {
+            disc.style.marginLeft = "-20vh";
+        });
+        disc.addEventListener("mouseclick", function () {
+            const projectTitle = document.getElementById("project-title");
+            projectTitle.innerHTML = projects_json_1.default.projects[1].title;
+        });
+        discs.appendChild(disc);
+    }
 }
